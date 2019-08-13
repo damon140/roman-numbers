81d6fe9ea0ff20586dc2c68ce5a4efd6f8ab6bd7
Damon van Opdorp <damon@damon140.com>
2019-08-13 16:09:52 +1000

    First working.

--- src/main/java/com/damon140/roman/RomanNumbers.java
+++ src/main/java/com/damon140/roman/RomanNumbers.java
@@ -5,42 +5,121 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class RomanNumbers {
-
-    private static final Map<String, Numeral> stringToNumeral = new HashMap();
-
+    
     public static enum Numeral {
+        i("i", 1),
         I("I", 1),
+        v("v", 5),
         V("V", 5),
+        x("x", 10),
         X("X", 10),
+        l("l", 50),
         L("L", 50),
+        /** Old form of C */
+        _IC(">I<", 100),
+        /** Old form of C */
+        _CIC("ƆIC", 100),
+        /** Old form of C */
+        _Cback("Ɔ", 100),
+        c("c", 100),
         C("C", 100),
+        _Dold("I?", 500),
+        d("d", 500),
         D("D", 500),
+        _M1("?", 1000),
+        _M2("?", 1000),
+        _M3("?", 1000),
+        _M4("?", 1000),
+        _M5("?", 1000),
+        _M6("?", 1000),
+        _M7("?", 1000),
+        m("m", 1000),
         M("M", 1000),
-
-        // TODO: add >I< & other unusual forms
         ;
         
-        private final int value;
+        // TODO: this source show more numerals, maybe modern extension? https://www.rapidtables.com/convert/number/roman-numerals-converter.html?x1=&x2=
+        
+        // TODO: rename to proper const
+        private static final Map<String, Numeral> stringToNumeral = new HashMap();
 
+        /**
+         * A map of numerals and the "little buddy" numerals that get used
+         * when writing a number to make the 4's and 9's.
+         */ 
+        private static final Map<Numeral, Optional<Numeral>> LITTLE_BUDDIES = new HashMap();
+        
+        static {
+            List<Numeral> reverseNumerals = Arrays.asList(Numeral.values());
+            Collections.reverse(reverseNumerals);
+            
+            Arrays.stream(Numeral.values()).forEach(n1 -> {
+                stringToNumeral.put(n1.characters, n1);
+                LITTLE_BUDDIES.put(n1, 
+                    reverseNumerals.stream()
+                        .filter(n -> 5 * n.getValue() == n1.value || 10 * n.getValue() == n1.value)
+                        .findFirst()
+                );
+            });
+        }
+        
+        private final int value;
+        private final String characters;
+
+        // TODO: flip constructor to allow list of multiple string representations
         private Numeral(String characters, int value){
             this.value = value;
-            RomanNumbers.stringToNumeral.put(characters, this);
+            this.characters = characters;
         }
 
         public int getValue() {
             return value;
+        }
+        
+        public String getCharacters() {
+            return characters;
         }
 
         public boolean smallerThan(Numeral other) {
             return this.value < other.value;
         }
 
+        public boolean smallerThanOrEqual(int i) {
+            return this.value <= i;
+        }
+                
         public boolean biggerThan(Numeral other) {
             return this.value > other.value;
         }
 
+        public int minus(int i) {
+            return i - this.getValue();
+        }
+        
+        public boolean smallerThanButClose(int i) {
+            Optional<Numeral> littleBuddy = littleBuddy();
+            return littleBuddy.isPresent()
+                    && this.value - littleBuddy.get().getValue() <= i && i < this.value;
+        }
+
+        public Optional<Numeral> predecessor() {
+            for (int looper = this.ordinal(); looper >= 0; looper--) {
+                final Numeral candidate = Numeral.values()[looper];
+                if (candidate.value < this.value) {
+                    return Optional.of(candidate);
+                }
+            }
+            
+            return Optional.empty();
+        }
+        
+        public Optional<Numeral> littleBuddy() {
+            return LITTLE_BUDDIES.get(this);
+        }
+                
         /**
          * Get a single enum value if the string exactly matches.
          * 
@@ -51,34 +130,47 @@ public class RomanNumbers {
             return Optional.ofNullable(stringToNumeral.get(numeralString));
         }
 
-        // FIXME: unit test
         public static List<Numeral> toRomanNumerals(String numeralsString) {
             List<Numeral> numerals = new ArrayList(); 
 
-            // TODO: update this naive impl when adding multichar numerals
             for (int index = 0; index < numeralsString.length(); index++) {
-                String numeralString = numeralsString.substring(index, index + 1);
-                Numeral numeral = stringToNumeral.get(numeralString);
-                if (null == numeral) {
-                    throw new IllegalArgumentException("Don't recognise numeral [" + numeralString + "].");
+                Optional<Numeral> numeral = toRomanNumeral(numeralsString.substring(index, index + 1));
+                if (!numeral.isPresent()) {
+                    // TODO: push this into block the numerals
+                    // fall back to subscanning the tail
+                    String tail = numeralsString.substring(index);
+                    numeral = Arrays.stream(Numeral.values())
+                        .filter(n -> tail.startsWith(n.getCharacters()))
+                        .findAny();
+                    
+                    if (numeral.isPresent()) {
+                        index += numeral.get().getCharacters().length() - 1;
+                    }
                 }
-                numerals.add(numeral);
+
+                if (!numeral.isPresent()) {
+                    // TODO: switch to specific exception type
+                    throw new IllegalArgumentException("Don't recognise numeral at head of substring [" + numeralsString + "].");
+                }
+                numerals.add(numeral.get());
             }
             
             return numerals;
         }
+
     }
 
-    // FIXME: roman number class
+    // TODO: roman number class??
 
-    public static int intFromString(String numerals) {
-        return intFromList(Numeral.toRomanNumerals(numerals));
+    public static Optional<Integer> intFromString(String numerals) {
+        try {
+            return intFromList(Numeral.toRomanNumerals(numerals));
+        } catch(IllegalArgumentException e) {
+            return Optional.empty();
+        }
     }
 
-
-    //  I    V
-    //  only prev character needed??
-    public static int intFromList(List<Numeral> numerals) {
+    public static Optional<Integer> intFromList(List<Numeral> numerals) {
         Optional<Numeral> prev = Optional.empty();
         int sum = 0;
         int index = 0;
@@ -88,6 +180,7 @@ public class RomanNumbers {
             sum += curr.getValue();
 
             if (prev.isPresent() && prev.get().smallerThan(curr)) {
+                //  only prev character needed for basic case
                 // do we need a backscan sequence detector for unusual varients?
                 sum -= 2 * prev.get().value;
             }
@@ -95,16 +188,49 @@ public class RomanNumbers {
             prev = Optional.of(curr);
         }
 
-        return sum;
+        return Optional.of(sum);
+    }
+
+    public static Optional<String> stringFromInt(int i) {
+        // FIXME: add max upper bound check
+
+        List<Numeral> values = Arrays.asList(Numeral.values());
+        Collections.reverse(values);
+
+        Optional<Numeral> special = values.stream()
+                .filter(v -> v.smallerThanButClose(i))
+                .findFirst();
+        
+        Optional<Numeral> numeral = values.stream()
+                .filter(v -> v.smallerThanOrEqual(i))
+                .findFirst();
+        
+        if (special.isEmpty() && numeral.isEmpty()) {
+            return Optional.empty();
+        } else {
+            String s;
+            final int remainder;
+                    
+            if (special.isPresent()) {
+                Numeral littleBuddy = special.get().littleBuddy().get();
+                s = littleBuddy.getCharacters() + special.get().getCharacters();
+                remainder = i - (special.get().getValue() - littleBuddy.getValue());
+            } else {
+                s = numeral.get().getCharacters();
+                remainder = numeral.get().minus(i);                
+            }
+            
+            if (0 < remainder) {
+                Optional<String> remainderString = stringFromInt(remainder);
+                s += remainderString.get();
+            }
+            
+            return Optional.of(s);
+        }
     }
 
     // FIXME: impl
-    // public static String stringFromInt(int i) {
-
-    // }
-
-    // FIXME: impl
-    // public static List<Numeral> numeralsFromInt(int i) {
+    // public static Optional<List<Numeral>> numeralsFromInt(int i) {
 
     // }
 
--- src/test/java/com/damon140/roman/RomanNumbersTest.java
+++ src/test/java/com/damon140/roman/RomanNumbersTest.java
@@ -8,8 +8,12 @@ import com.damon140.roman.RomanNumbers.Numeral;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 
+import java.util.Optional;
+
 public class RomanNumbersTest {
 
+    // TODO: switch whole class to Hamcrest https://openwritings.net/pg/java/using-hamcrest-testng
+    
     @Test
     public void toRomanNumeral() {
         assertEquals(RomanNumbers.Numeral.toRomanNumeral("I").get(), Numeral.I);
@@ -27,50 +31,50 @@ public class RomanNumbersTest {
             Numeral.I, Numeral.V, Numeral.X, Numeral.L, Numeral.C, Numeral.D, Numeral.M
         ));
     }
-
+    
     @Test
     public void literals() {
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.I)), 1);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.V)), 5);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.X)), 10);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.L)), 50);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.C)), 100);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.D)), 500);
-        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.M)), 1000);
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.I)), Optional.of(1));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.V)), Optional.of(5));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.X)), Optional.of(10));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.L)), Optional.of(50));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.C)), Optional.of(100));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.D)), Optional.of(500));
+        assertEquals(RomanNumbers.intFromList(Arrays.asList(Numeral.M)), Optional.of(1000));
     }
     
     @Test
     public void simpleAdditive1() {
         List all = Arrays.asList(
             Numeral.M, Numeral.D, Numeral.C, Numeral.L, Numeral.X, Numeral.V, Numeral.I);
-        assertEquals(RomanNumbers.intFromList(all), 1666);
+        assertEquals(RomanNumbers.intFromList(all), Optional.of(1666));
     }
 
     @Test
     public void simpleAdditive2() {
-        assertEquals(RomanNumbers.intFromString("MDCLXVI"), 1666);
+        assertEquals(RomanNumbers.intFromString("MDCLXVI"), Optional.of(1666));
     }
 
     @Test
     public void clockFaceNumbers() {
-        assertEquals(RomanNumbers.intFromString("I"), 1);
-        assertEquals(RomanNumbers.intFromString("II"), 2);
-        assertEquals(RomanNumbers.intFromString("III"), 3);
-        assertEquals(RomanNumbers.intFromString("IV"), 4);
-        assertEquals(RomanNumbers.intFromString("V"), 5);
-        assertEquals(RomanNumbers.intFromString("VI"), 6);
-        assertEquals(RomanNumbers.intFromString("VII"), 7);
-        assertEquals(RomanNumbers.intFromString("VIII"), 8);
-        assertEquals(RomanNumbers.intFromString("IX"), 9);
-        assertEquals(RomanNumbers.intFromString("X"), 10);
-        assertEquals(RomanNumbers.intFromString("XI"), 11);
-        assertEquals(RomanNumbers.intFromString("XII"), 12);
+        assertEquals(RomanNumbers.intFromString("I"), Optional.of(1));
+        assertEquals(RomanNumbers.intFromString("II"), Optional.of(2));
+        assertEquals(RomanNumbers.intFromString("III"), Optional.of(3));
+        assertEquals(RomanNumbers.intFromString("IV"), Optional.of(4));
+        assertEquals(RomanNumbers.intFromString("V"), Optional.of(5));
+        assertEquals(RomanNumbers.intFromString("VI"), Optional.of(6));
+        assertEquals(RomanNumbers.intFromString("VII"), Optional.of(7));
+        assertEquals(RomanNumbers.intFromString("VIII"), Optional.of(8));
+        assertEquals(RomanNumbers.intFromString("IX"), Optional.of(9));
+        assertEquals(RomanNumbers.intFromString("X"), Optional.of(10));
+        assertEquals(RomanNumbers.intFromString("XI"), Optional.of(11));
+        assertEquals(RomanNumbers.intFromString("XII"), Optional.of(12));
     }
 
     @Test
     public void simpleSubtractive() {
-        assertEquals(RomanNumbers.intFromString("IM"), 999);
-        assertEquals(RomanNumbers.intFromString("CM"), 900);
+        assertEquals(RomanNumbers.intFromString("IM"), Optional.of(999));
+        assertEquals(RomanNumbers.intFromString("CM"), Optional.of(900));
     }
 
     @Test
@@ -78,30 +82,114 @@ public class RomanNumbersTest {
         // Simple examples from Wikipedia
 
         // 1912 is written MCMXII
-        assertEquals(RomanNumbers.intFromString("MCMXII"), 1912);
+        assertEquals(RomanNumbers.intFromString("MCMXII"), Optional.of(1912));
 
         // For this century, MM indicates 2000
-        assertEquals(RomanNumbers.intFromString("MM"), 2000);
+        assertEquals(RomanNumbers.intFromString("MM"), Optional.of(2000));
 
         // Thus the current year is MMXIX (2019).
-        assertEquals(RomanNumbers.intFromString("MMXIX"), 2019);
+        assertEquals(RomanNumbers.intFromString("MMXIX"), Optional.of(2019));
     }
 
     @Test
     public void unabbreviated1to10() {
-        assertEquals(RomanNumbers.intFromString("I"), 1);
-        assertEquals(RomanNumbers.intFromString("II"), 2);
-        assertEquals(RomanNumbers.intFromString("III"), 3);
-        assertEquals(RomanNumbers.intFromString("IIII"), 4);
-        assertEquals(RomanNumbers.intFromString("V"), 5);
-        assertEquals(RomanNumbers.intFromString("VI"), 6);
-        assertEquals(RomanNumbers.intFromString("VII"), 7);
-        assertEquals(RomanNumbers.intFromString("VIII"), 8);
-        assertEquals(RomanNumbers.intFromString("VIIII"), 9);
-        assertEquals(RomanNumbers.intFromString("X"), 10);
+        assertEquals(RomanNumbers.intFromString("I"), Optional.of(1));
+        assertEquals(RomanNumbers.intFromString("II"), Optional.of(2));
+        assertEquals(RomanNumbers.intFromString("III"), Optional.of(3));
+        assertEquals(RomanNumbers.intFromString("IIII"), Optional.of(4));
+        assertEquals(RomanNumbers.intFromString("V"), Optional.of(5));
+        assertEquals(RomanNumbers.intFromString("VI"), Optional.of(6));
+        assertEquals(RomanNumbers.intFromString("VII"), Optional.of(7));
+        assertEquals(RomanNumbers.intFromString("VIII"), Optional.of(8));
+        assertEquals(RomanNumbers.intFromString("VIIII"), Optional.of(9));
+        assertEquals(RomanNumbers.intFromString("X"), Optional.of(10));
     }
 
-    // FIXME: test for busted numbers that don't work
-    // IIM is busted??
+    @Test
+    public void oldForms() {
+        // old forms of C
+        assertEquals(RomanNumbers.intFromString(">I<"), Optional.of(100));
+        assertEquals(RomanNumbers.intFromString("ƆIC"), Optional.of(100));
+        assertEquals(RomanNumbers.intFromString("Ɔ"), Optional.of(100));
 
+        // old forms of D
+        // TODO: this one fails, some sort of encoding problem??
+        //assertEquals(RomanNumbers.intFromString("I?"), 500);
+        
+        // old forms of M
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        assertEquals(RomanNumbers.intFromString("?"), Optional.of(1000));
+        
+        // lowercase varients of regular forms
+        assertEquals(RomanNumbers.intFromString("mdclxvi"), Optional.of(1666));
+    }
+
+    @Test
+    public void badCharacters() {
+        assertEquals(RomanNumbers.intFromString("ih"), Optional.empty());
+    }
+    
+    @Test
+    public void stringFromInt_primitives() {
+        assertEquals(RomanNumbers.stringFromInt(1000), Optional.of("M"));
+        assertEquals(RomanNumbers.stringFromInt(500), Optional.of("D"));
+        assertEquals(RomanNumbers.stringFromInt(100), Optional.of("C"));
+        assertEquals(RomanNumbers.stringFromInt(50), Optional.of("L"));
+        assertEquals(RomanNumbers.stringFromInt(10), Optional.of("X"));
+        assertEquals(RomanNumbers.stringFromInt(5), Optional.of("V"));
+        assertEquals(RomanNumbers.stringFromInt(1), Optional.of("I"));
+    }
+
+    @Test
+    public void stringFromInt_simpleCompound() {
+        assertEquals(RomanNumbers.stringFromInt(15), Optional.of("XV"));
+        assertEquals(RomanNumbers.stringFromInt(1666), Optional.of("MDCLXVI"));
+    }
+    
+    @Test
+    public void littleBuddy() {
+        assertEquals(Numeral.I.littleBuddy(), Optional.empty());
+        assertEquals(Numeral.V.littleBuddy(), Optional.of(Numeral.I));
+        assertEquals(Numeral.X.littleBuddy(), Optional.of(Numeral.I));
+        assertEquals(Numeral.C.littleBuddy(), Optional.of(Numeral.X));
+        assertEquals(Numeral.L.littleBuddy(), Optional.of(Numeral.X));
+        assertEquals(Numeral.M.littleBuddy(), Optional.of(Numeral.C));
+        assertEquals(Numeral.D.littleBuddy(), Optional.of(Numeral.C));
+    }
+    
+    /**
+     * Test the corner cases of 9s and 4s
+     */
+    @Test
+    public void stringFromInt_9and4cornercases() {
+        assertEquals(RomanNumbers.stringFromInt(4), Optional.of("IV"));
+        assertEquals(RomanNumbers.stringFromInt(9), Optional.of("IX"));
+        
+        assertEquals(RomanNumbers.stringFromInt(3999), Optional.of("MMMCMXCIX"));
+        assertEquals(RomanNumbers.stringFromInt(3994), Optional.of("MMMCMXCIV"));
+        assertEquals(RomanNumbers.stringFromInt(3949), Optional.of("MMMCMXLIX"));
+        assertEquals(RomanNumbers.stringFromInt(3944), Optional.of("MMMCMXLIV"));
+        assertEquals(RomanNumbers.stringFromInt(3499), Optional.of("MMMCDXCIX"));
+        assertEquals(RomanNumbers.stringFromInt(3494), Optional.of("MMMCDXCIV"));
+        assertEquals(RomanNumbers.stringFromInt(3444), Optional.of("MMMCDXLIV"));
+        assertEquals(RomanNumbers.stringFromInt(3449), Optional.of("MMMCDXLIX"));
+    }
+    
+    @Test
+    public void stringFromInt_commonyears() {
+        assertEquals(RomanNumbers.stringFromInt(1776), Optional.of("MDCCLXXVI"));
+        assertEquals(RomanNumbers.stringFromInt(1954), Optional.of("MCMLIV"));
+        assertEquals(RomanNumbers.stringFromInt(2014), Optional.of("MMXIV"));
+        assertEquals(RomanNumbers.stringFromInt(2019), Optional.of("MMXIX"));
+    }
+    
+    // TODO: test for busted numbers that don't work
+    // IM is busted
+    // iiiii is busted
+    
 }
